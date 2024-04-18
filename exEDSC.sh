5392cfc417bc8fdf1dc619fcc44d2962b081143f
Kazuya Erdos <kerdos@cs.brown.edu>
2024-04-18 15:06:36 -0400

    stuff

--- exEDSC.sh
+++ exEDSC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:8
-#SBATCH --output=output_file.log  # Specify the output file
+# #SBATCH --output=output_file.log  # Specify the output file
 
 if command -v nvidia-smi &> /dev/null; then
 	    echo "NVIDIA GPUs found:"
@@ -11,15 +11,17 @@ if command -v nvidia-smi &> /dev/null; then
 fi
 start=`date +%s`
 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.1 --out frames/out1.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.2 --out frames/out2.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.3 --out frames/out3.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.4 --out frames/out4.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.5 --out frames/out5.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.6 --out frames/out6.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.7 --out frames/out7.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.8 --out frames/out8.png 2>&1
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.9 --out frames/out9.png 2>&1
+echo "running python scripts"
+
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.1 --out frames/out1.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.2 --out frames/out2.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.3 --out frames/out3.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.4 --out frames/out4.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.5 --out frames/out5.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.6 --out frames/out6.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.7 --out frames/out7.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.8 --out frames/out8.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.9 --out frames/out9.png
 
 end=`date +%s`
 
