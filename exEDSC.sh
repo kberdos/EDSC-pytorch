36a70058161a6a0f746f966599558648953346db
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-04-18 17:16:57 -0400

    more frames

--- exEDSC.sh
+++ exEDSC.sh
@@ -12,15 +12,25 @@ start=`date +%s`
 
 echo "running python scripts"
 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.1 --out frames/out1.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.2 --out frames/out2.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.3 --out frames/out3.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.4 --out frames/out4.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.5 --out frames/out5.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.6 --out frames/out6.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.7 --out frames/out7.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.8 --out frames/out8.png 
-python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.9 --out frames/out9.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 0.5 --out frames/out1.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 1.0 --out frames/out2.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 1.5 --out frames/out3.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 2.0 --out frames/out4.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 2.5 --out frames/out5.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 3.0 --out frames/out6.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 3.5 --out frames/out7.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 4.0 --out frames/out8.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 4.5 --out frames/out9.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 5.0 --out frames/out10.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 5.5 --out frames/out11.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 6.0 --out frames/out12.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 6.5 --out frames/out13.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 7.0 --out frames/out14.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 7.5 --out frames/out15.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 8.0 --out frames/out16.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 8.5 --out frames/out17.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 9.0 --out frames/out18.png 
+python3 run.py --model EDSC_m --model_state EDSC_m.ckpt --time 9.5 --out frames/out19.png
 
 end=`date +%s`
 
